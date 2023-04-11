7ada0d8220984ac203a3fa5ad0387e2ab5330421
João Alves <joao.f.alves@wit-software.com>
2023-04-11 10:22:45 +0100

    add: web port configurable

--- server/main.py
+++ server/main.py
@@ -189,4 +189,4 @@ async def startup():
 
 
 def start():
-    uvicorn.run("server.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("server.main:app", host="0.0.0.0", port=int(os.getenv('WEB_PORT', 8000)), reload=True)
